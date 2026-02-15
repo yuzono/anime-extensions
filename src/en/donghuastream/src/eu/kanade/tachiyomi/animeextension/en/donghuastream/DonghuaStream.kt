9fcb38aac550ab2a9cefb00cb61c46f0f1b6b6cf
Bilal <bjlegend007@gmail.com>
2026-02-15 16:54:43 +0530

    tidying up the file

--- src/en/donghuastream/src/eu/kanade/tachiyomi/animeextension/en/donghuastream/DonghuaStream.kt
+++ src/en/donghuastream/src/eu/kanade/tachiyomi/animeextension/en/donghuastream/DonghuaStream.kt
@@ -1,13 +1,10 @@
 package eu.kanade.tachiyomi.animeextension.en.donghuastream
 
 import eu.kanade.tachiyomi.animeextension.en.donghuastream.extractors.StreamPlayExtractor
-import eu.kanade.tachiyomi.animesource.model.SEpisode
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.lib.dailymotionextractor.DailymotionExtractor
 import eu.kanade.tachiyomi.lib.okruextractor.OkruExtractor
 import eu.kanade.tachiyomi.multisrc.animestream.AnimeStream
-import eu.kanade.tachiyomi.util.asJsoup
-import okhttp3.Response
 
 class DonghuaStream :
     AnimeStream(
@@ -22,15 +19,17 @@ class DonghuaStream :
 
     private val dailymotionExtractor by lazy { DailymotionExtractor(client, headers) }
     private val streamPlayExtractor by lazy { StreamPlayExtractor(client, headers) }
-
     private val okRuPlayExtractor by lazy { OkruExtractor(client) }
 
     override fun getVideoList(url: String, name: String): List<Video> {
         val prefix = "$name - "
         return when {
-            url.contains("dailymotion") -> dailymotionExtractor.videosFromUrl(url, prefix = prefix)
-            url.contains("streamplay") -> streamPlayExtractor.videosFromUrl(url, prefix = prefix)
-            url.contains("ok.ru") -> okRuPlayExtractor.videosFromUrl(url, prefix = prefix)
+            url.contains("dailymotion") ->
+                dailymotionExtractor.videosFromUrl(url, prefix = prefix)
+            url.contains("streamplay") ->
+                streamPlayExtractor.videosFromUrl(url, prefix = prefix)
+            url.contains("ok.ru") ->
+                okRuPlayExtractor.videosFromUrl(url, prefix = prefix)
             else -> emptyList()
         }
     }
