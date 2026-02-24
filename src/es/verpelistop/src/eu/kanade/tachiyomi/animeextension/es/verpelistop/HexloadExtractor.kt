96a158f1673c08af87e852c2d50500fd3a0a1039
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-02-25 00:15:29 +0700

    Fix HexloadExtractor

--- src/es/verpelistop/src/eu/kanade/tachiyomi/animeextension/es/verpelistop/HexloadExtractor.kt
+++ src/es/verpelistop/src/eu/kanade/tachiyomi/animeextension/es/verpelistop/HexloadExtractor.kt
@@ -14,7 +14,7 @@ class HexloadExtractor(private val client: OkHttpClient, private val headers: He
     // Example URL: https://hexload.com/embed-ii21t0gfa9c3/Avatar_Fuego_y_ceniza_LAT
     private val idRegex by lazy { Regex("""embed-(\w*?)/""") }
 
-    suspend fun videosFromUrl(url: String, prefix: String = ""): List<Video> {
+    suspend fun videosFromUrl(url: String, prefix: String = "Hexload"): List<Video> {
         val id = idRegex.find(url)?.groupValues?.getOrNull(1) ?: return emptyList()
 
         val formBody = FormBody.Builder()
@@ -33,14 +33,24 @@ class HexloadExtractor(private val client: OkHttpClient, private val headers: He
             .add("X-Requested-With", "XMLHttpRequest")
             .build()
 
-        val videoUrl = client.newCall(
+        val video = client.newCall(
             POST("https://hexload.com/download", headers, body = formBody),
-        ).awaitSuccess().parseAs<Data>().result.url ?: return emptyList()
+        ).awaitSuccess().parseAs<Data>().result
 
-        val videoHeaders = Headers.headersOf("Referer", "https://hexload.com/")
-        val quality = if (prefix.isNotBlank()) "$prefix Hexload" else "Hexload"
+        val quality = formatBytes(video.size).takeIf { it.isNotBlank() }
+            ?.let { "$prefix: $it" }
+            ?: prefix
 
-        return listOf(Video(videoUrl, quality, videoUrl, videoHeaders))
+        return listOf(Video(video.url, quality, video.url, headers))
+    }
+
+    private fun formatBytes(bytes: Long): String = when {
+        bytes >= 1_000_000_000 -> "%.2f GB".format(bytes / 1_000_000_000.0)
+        bytes >= 1_000_000 -> "%.2f MB".format(bytes / 1_000_000.0)
+        bytes >= 1_000 -> "%.2f KB".format(bytes / 1_000.0)
+        bytes > 1 -> "$bytes bytes"
+        bytes == 1L -> "$bytes byte"
+        else -> ""
     }
 
     @Serializable
@@ -50,11 +60,11 @@ class HexloadExtractor(private val client: OkHttpClient, private val headers: He
 
     @Serializable
     private data class ResultDto(
-        val url: String?,
+        val url: String,
         val md5: String?, // "VPHYYxNMLxKIxBZWE9hl+A",
         val thumb_url: String?, // "https://46ev7agtixoi.droply.top/i/01264/pbv78rlcsagr_t.jpg",
         val content_type: String?, // "video/mp4",
-        val size: String?, // "3065200640",
+        val size: Long, // "3065200640",
         val image_url: String?, // "https://46ev7agtixoi.droply.top/i/01264/pbv78rlcsagr.jpg",
         val folder: String?, // null,
         val file_name: String?, // "Avatar Fuego y ceniza LAT.mp4"
