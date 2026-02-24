83dc59f75776765b344eee1fa5da808578a8fd28
WebDitto <webditto@proton.me>
2026-02-24 12:07:33 -0300

    Anime Onsen: Fixed search (#1235)
    (cherry picked from commit be1af14d24ed3dbd7d863776389d853f60d81f61)

--- src/all/animeonsen/src/eu/kanade/tachiyomi/animeextension/all/animeonsen/AnimeOnsen.kt
+++ src/all/animeonsen/src/eu/kanade/tachiyomi/animeextension/all/animeonsen/AnimeOnsen.kt
@@ -17,12 +17,19 @@ import eu.kanade.tachiyomi.animesource.model.Track
 import eu.kanade.tachiyomi.animesource.model.Video
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.network.GET
+import eu.kanade.tachiyomi.network.POST
 import keiyoushi.utils.getPreferencesLazy
 import keiyoushi.utils.parseAs
+import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.boolean
+import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.put
 import okhttp3.Headers
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
 import okhttp3.Response
 import uy.kohesive.injekt.injectLazy
 
@@ -36,13 +43,16 @@ class AnimeOnsen :
 
     private val apiUrl = "https://api.animeonsen.xyz/v4"
 
+    private val searchUrl = "https://search.animeonsen.xyz"
+
     override val lang = "all"
 
     override val supportsLatest = false
 
     override val client by lazy {
         network.client.newBuilder()
-            .addInterceptor(AOAPIInterceptor(network.client))
+            .addInterceptor(AOAPIInterceptor(network.client, apiUrl))
+            .addInterceptor(SearchInterceptor(network.client, baseUrl, searchUrl))
             .build()
     }
 
@@ -70,10 +80,18 @@ class AnimeOnsen :
     override fun latestUpdatesParse(response: Response) = throw UnsupportedOperationException()
 
     // =============================== Search ===============================
-    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList) = GET("$apiUrl/search/$query")
+    override fun searchAnimeRequest(page: Int, query: String, filters: AnimeFilterList): Request {
+        val postBody = json.encodeToString(
+            buildJsonObject {
+                put("q", query)
+            },
+        ).toRequestBody("application/json; charset=utf-8".toMediaType())
+
+        return POST("$searchUrl/indexes/content/search", body = postBody)
+    }
 
     override fun searchAnimeParse(response: Response): AnimesPage {
-        val searchResult = response.parseAs<SearchResponse>().result
+        val searchResult = response.parseAs<SearchResponse>().hits
         val results = searchResult.map { it.toSAnime() }
         return AnimesPage(results, false)
     }
