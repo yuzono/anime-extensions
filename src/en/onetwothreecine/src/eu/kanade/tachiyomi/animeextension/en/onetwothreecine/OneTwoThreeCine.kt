1470655499bf2912a6e671dbcaa79ccdcf940d3d
Alpha-782 <georgesgeorges782@gmail.com>
2026-04-27 16:49:34 +0300

    Remove unused imports

--- src/en/onetwothreecine/src/eu/kanade/tachiyomi/animeextension/en/onetwothreecine/OneTwoThreeCine.kt
+++ src/en/onetwothreecine/src/eu/kanade/tachiyomi/animeextension/en/onetwothreecine/OneTwoThreeCine.kt
@@ -24,14 +24,12 @@ import keiyoushi.utils.parallelMapNotNull
 import keiyoushi.utils.parseAs
 import okhttp3.Headers
 import okhttp3.HttpUrl.Companion.toHttpUrl
-import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
 import uy.kohesive.injekt.injectLazy
 import java.math.BigDecimal
 import java.math.RoundingMode
 import kotlin.getValue
-import kotlin.time.Duration.Companion.seconds
 import java.util.concurrent.TimeUnit
 
 class OneTwoThreeCine :
