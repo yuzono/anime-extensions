Kohi-den/extensions-source

fix(flixlatam): Enhance video extraction and link decryption methods (#1093)

src/es/flixlatam/src/eu/kanade/tachiyomi/animeextension/es/flixlatam/FlixLatam.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.animeextension.es.flixlatam
 
+import android.util.Base64
 import android.util.Log
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
@@ -17,7 +18,6 @@ import eu.kanade.tachiyomi.lib.streamlareextractor.StreamlareExtractor
 import eu.kanade.tachiyomi.lib.streamsilkextractor.StreamSilkExtractor
 import eu.kanade.tachiyomi.lib.streamtapeextractor.StreamTapeExtractor
 import eu.kanade.tachiyomi.lib.streamwishextractor.StreamWishExtractor
-import eu.kanade.tachiyomi.lib.universalextractor.UniversalExtractor
 import eu.kanade.tachiyomi.lib.upstreamextractor.UpstreamExtractor
 import eu.kanade.tachiyomi.lib.uqloadextractor.UqloadExtractor
 import eu.kanade.tachiyomi.lib.vidguardextractor.VidGuardExtractor
@@ -30,17 +30,25 @@ import eu.kanade.tachiyomi.util.asJsoup
 import eu.kanade.tachiyomi.util.parallelFlatMapBlocking
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.FormBody
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import java.net.URLDecoder
+import kotlin.text.RegexOption
 
 class FlixLatam : DooPlay(
     "es",
     "FlixLatam",
     "https://flixlatam.com",
 ) {
+    private val json by lazy { Json { ignoreUnknownKeys = true } }
+
     override fun popularAnimeRequest(page: Int) = GET("$baseUrl/pelicula/page/$page")
 
     override fun popularAnimeSelector() = latestUpdatesSelector()
@@ -61,16 +69,25 @@ class FlixLatam : DooPlay(
         val document = response.asJsoup()
         val videoSet = mutableSetOf<Video>()
         val players = document.select("ul#playeroptionsul li")
+        val referer = response.request.url.toString()
+        val embedHeaders = headersBuilder().set("Referer", referer).build()
 
         players.parallelFlatMapBlocking { player ->
             val url = getPlayerUrl(player)
                 ?: return@parallelFlatMapBlocking emptyList<Video>()
             Log.i("bruh url", url)
             if (url.contains("embed69")) {
-                val htmlContent = client.newCall(GET(url)).execute().body.string()
-                val links = extractNewExtractorLinks(document, htmlContent) ?: return@parallelFlatMapBlocking emptyList<Video>()
-                links.forEach { (link, language) ->
-                    videoSet.addAll(serverVideoResolver(link, " $language"))
+                client.newCall(GET(url, embedHeaders)).execute().use { res ->
+                    val htmlContent = res.body.string()
+                    if (htmlContent.isBlank()) return@parallelFlatMapBlocking emptyList()
+
+                    val embedDoc = Jsoup.parse(htmlContent)
+                    val links = extractNewExtractorLinks(embedDoc, htmlContent)
+                        ?: return@parallelFlatMapBlocking emptyList<Video>()
+
+                    links.forEach { (link, language) ->
+                        videoSet.addAll(serverVideoResolver(link, " $language"))
+                    }
                 }
             }
             return@parallelFlatMapBlocking emptyList()
@@ -88,11 +105,11 @@ class FlixLatam : DooPlay(
             .build()
 
         return client.newCall(POST("$baseUrl/wp-admin/admin-ajax.php", headers, body))
-            .execute().body?.string()
-            ?.substringAfter("\"embed_url\":\"")
-            ?.substringBefore("\",")
-            ?.replace("\\", "")
-            ?.takeIf(String::isNotBlank)
+            .execute().body.string()
+            .substringAfter("\"embed_url\":\"")
+            .substringBefore("\",")
+            .replace("\\", "")
+            .takeIf(String::isNotBlank)
     }
 
     /*-------------------------------- Video extractors ------------------------------------*/
@@ -112,7 +129,6 @@ class FlixLatam : DooPlay(
     private val streamHideVidExtractor by lazy { StreamHideVidExtractor(client, headers) }
     private val streamSilkExtractor by lazy { StreamSilkExtractor(client) }
     private val vidGuardExtractor by lazy { VidGuardExtractor(client) }
-    private val universalExtractor by lazy { UniversalExtractor(client) }
 
     private fun serverVideoResolver(url: String, prefix: String = ""): List<Video> {
         return runCatching {
@@ -180,16 +196,36 @@ class FlixLatam : DooPlay(
 
     private fun extractNewExtractorLinks(doc: Document, htmlContent: String): List<Pair<String, String>>? {
         val links = mutableListOf<Pair<String, String>>()
-        val jsLinksMatch = getFirstMatch("""dataLink = (\[.+?\]);""".toRegex(), htmlContent) ?: return null
 
-        val items = Json.decodeFromString<List<Item>>(jsLinksMatch)
+        val scriptData = doc.select("script")
+            .asSequence()
+            .map(Element::data)
+            .firstOrNull { it.contains("dataLink") }
+
+        val rawExpression = scriptData?.let {
+            getFirstMatch(DATA_LINK_REGEX, it)
+        } ?: getFirstMatch(DATA_LINK_REGEX, htmlContent)
+
+        val jsonPayload = resolveDataLink(rawExpression) ?: return null
+
+        val items = runCatching {
+            json.decodeFromString<List<Item>>(jsonPayload)
+        }.getOrElse {
+            Log.e("FlixLatam", "No se pudo parsear dataLink", it)
+            return null
+        }
+
         val idiomas = mapOf("LAT" to "[LAT]", "ESP" to "[CAST]", "SUB" to "[SUB]")
 
         items.forEach { item ->
-            val languageCode = idiomas[item.video_language] ?: "unknown"
+            val languageKey = item.video_language?.uppercase() ?: ""
+            val languageCode = idiomas[languageKey] ?: "unknown"
+
             item.sortedEmbeds.forEach { embed ->
-                val decryptedLink = CryptoAES.decrypt(embed.link, "Ak7qrvvH4WKYxV2OgaeHAEg2a5eh16vE")
-                links.add(Pair(decryptedLink, languageCode))
+                if (!"video".equals(embed.type, ignoreCase = true)) return@forEach
+
+                val decryptedLink = decryptEmbedLink(embed.link)
+                decryptedLink?.let { links.add(it to languageCode) }
             }
         }
 
@@ -198,6 +234,93 @@ class FlixLatam : DooPlay(
 
     private fun getFirstMatch(regex: Regex, input: String): String? {
         return regex.find(input)?.groupValues?.get(1)
+    }
+
+    private fun resolveDataLink(rawExpression: String?): String? {
+        if (rawExpression.isNullOrBlank()) return null
+
+        var expr = rawExpression.trim().trimEnd(';')
+
+        fun String.removeOuterCall(prefix: String): String? {
+            if (!startsWith(prefix, ignoreCase = true) || !endsWith(')')) return null
+            val start = indexOf('(')
+            val end = lastIndexOf(')')
+            if (start == -1 || end == -1 || end <= start) return null
+            return substring(start + 1, end).trim()
+        }
+
+        fun String.trimMatchingQuotes(): String {
+            return if ((startsWith('"') && endsWith('"')) || (startsWith('\'') && endsWith('\''))) {
+                substring(1, length - 1)
+            } else {
+                this
+            }
+        }
+
+        while (true) {
+            when {
+                expr.removeOuterCall("JSON.parse") != null -> expr = expr.removeOuterCall("JSON.parse")!!
+                expr.removeOuterCall("window.JSON.parse") != null -> expr = expr.removeOuterCall("window.JSON.parse")!!
+                expr.removeOuterCall("decodeURIComponent") != null -> {
+                    val inner = expr.removeOuterCall("decodeURIComponent")!!.trimMatchingQuotes()
+                    expr = runCatching { URLDecoder.decode(inner, "UTF-8") }.getOrElse { return null }
+                }
+                expr.removeOuterCall("window.decodeURIComponent") != null -> {
+                    val inner = expr.removeOuterCall("window.decodeURIComponent")!!.trimMatchingQuotes()
+                    expr = runCatching { URLDecoder.decode(inner, "UTF-8") }.getOrElse { return null }
+                }
+                expr.removeOuterCall("atob") != null -> {
+                    val inner = expr.removeOuterCall("atob")!!.trimMatchingQuotes()
+                    expr = runCatching { String(Base64.decode(inner, Base64.DEFAULT)) }.getOrElse { return null }
+                }
+                expr.removeOuterCall("window.atob") != null -> {
+                    val inner = expr.removeOuterCall("window.atob")!!.trimMatchingQuotes()
+                    expr = runCatching { String(Base64.decode(inner, Base64.DEFAULT)) }.getOrElse { return null }
+                }
+                else -> break
+            }
+        }
+
+        expr = expr.trim().trimMatchingQuotes()
+
+        return expr.takeIf { it.isNotBlank() }
+    }
+
+    private fun decryptEmbedLink(rawLink: String?): String? {
+        if (rawLink.isNullOrBlank()) return null
+
+        val link = rawLink.trim()
+        if (link.startsWith("http", true)) return link
+
+        CryptoAES.decryptCbcIV(link, AES_KEY)?.takeIf { it.isNotBlank() }?.let { return it }
+        CryptoAES.decrypt(link, AES_KEY).takeIf { it.isNotBlank() }?.let { return it }
+
+        decodeJwtLink(link)?.takeIf { it.isNotBlank() }?.let { return it }
+
+        return null
+    }
+
+    private fun decodeJwtLink(token: String): String? {
+        val segments = token.split('.')
+        if (segments.size < 2) return null
+
+        val payload = segments[1].padBase64Url()
+
+        return runCatching {
+            val decoded = Base64.decode(payload, Base64.URL_SAFE or Base64.NO_WRAP)
+            val element = json.parseToJsonElement(String(decoded))
+            val obj = element.jsonObject
+
+            val link = obj["link"]?.jsonPrimitive?.contentOrNull
+            val nestedLink = obj["data"]?.jsonObject?.get("link")?.jsonPrimitive?.contentOrNull
+
+            link ?: nestedLink
+        }.getOrNull()
+    }
+
+    private fun String.padBase64Url(): String {
+        val padding = (4 - length % 4) % 4
+        return this + "=".repeat(padding)
     }
 
     // ============================== Filters ===============================
@@ -275,19 +398,18 @@ class FlixLatam : DooPlay(
 
     @Serializable
     data class Item(
-        val file_id: Int,
-        val video_language: String, // Campo nuevo para almacenar el idioma
-        val sortedEmbeds: List<Embed>,
+        val file_id: Int? = null,
+        val video_language: String? = null,
+        val sortedEmbeds: List<Embed> = emptyList(),
     )
 
     @Serializable
     data class Embed(
-        val servername: String,
-        val link: String,
-        val type: String,
+        val servername: String? = null,
+        val link: String? = null,
+        val type: String? = null,
+        val download: String? = null,
     )
-
-    private fun Array<String>.any(url: String): Boolean = this.any { url.contains(it, ignoreCase = true) }
 
     override fun List<Video>.sort(): List<Video> {
         val quality = preferences.getString(prefQualityKey, prefQualityDefault)!!
@@ -314,5 +436,7 @@ class FlixLatam : DooPlay(
         private val PREF_LANG_ENTRIES = arrayOf("[LAT]", "[SUB]", "[CAST]")
         private val PREF_LANG_VALUES = arrayOf("[LAT]", "[SUB]", "[CAST]")
         private val SERVER_LIST = arrayOf("StreamWish", "Uqload", "VidGuard", "StreamHideVid", "Voe")
+        private const val AES_KEY = "Ak7qrvvH4WKYxV2OgaeHAEg2a5eh16vE"
+        private val DATA_LINK_REGEX = """dataLink\s*=\s*([^;]+);""".toRegex(RegexOption.DOT_MATCHES_ALL)
     }
 }

yuzono/anime-extensions

(cherry picked from commit de2152d6ddf62ba796cd07a88580d302b599218e)

src/es/flixlatam/src/eu/kanade/tachiyomi/animeextension/es/flixlatam/FlixLatam.kt
@@ -1,5 +1,6 @@
 package eu.kanade.tachiyomi.animeextension.es.flixlatam
 
+import android.util.Base64
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceScreen
 import aniyomi.lib.burstcloudextractor.BurstCloudExtractor
@@ -32,10 +33,18 @@ import keiyoushi.utils.parallelCatchingFlatMapBlocking
 import keiyoushi.utils.parseAs
 import keiyoushi.utils.useAsJsoup
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.contentOrNull
+import kotlinx.serialization.json.jsonObject
+import kotlinx.serialization.json.jsonPrimitive
 import okhttp3.FormBody
 import okhttp3.Request
 import okhttp3.Response
+import org.jsoup.Jsoup
+import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
+import java.net.URLDecoder
+import kotlin.text.RegexOption
 
 class FlixLatam :
     DooPlay(
@@ -43,6 +52,8 @@ class FlixLatam :
         "FlixLatam",
         "https://flixlatam.com",
     ) {
+    private val json by lazy { Json { ignoreUnknownKeys = true } }
+
     override fun popularAnimeRequest(page: Int) = GET("$baseUrl/pelicula/page/$page")
 
     override fun popularAnimeSelector() = latestUpdatesSelector()
@@ -62,13 +73,20 @@ class FlixLatam :
     override fun videoListParse(response: Response): List<Video> {
         val document = response.asJsoup()
         val players = document.select("ul#playeroptionsul li")
+        val referer = response.request.url.toString()
+        val embedHeaders = headersBuilder().set("Referer", referer).build()
 
         return players.parallelCatchingFlatMapBlocking { player ->
             val url = getPlayerUrl(player)
                 ?: return@parallelCatchingFlatMapBlocking emptyList()
             if (url.contains("embed69")) {
-                val htmlContent = client.newCall(GET(url)).awaitSuccess().bodyString()
-                val links = extractNewExtractorLinks(htmlContent) ?: return@parallelCatchingFlatMapBlocking emptyList()
+                val htmlContent = client.newCall(GET(url, embedHeaders)).awaitSuccess().bodyString()
+                if (htmlContent.isBlank()) return@parallelCatchingFlatMapBlocking emptyList()
+
+                val embedDoc = Jsoup.parse(htmlContent)
+                val links = extractNewExtractorLinks(embedDoc, htmlContent)
+                    ?: return@parallelCatchingFlatMapBlocking emptyList<Video>()
+
                 links.parallelCatchingFlatMap { (link, language) ->
                     serverVideoResolver(link, " $language")
                 }
@@ -174,24 +192,124 @@ class FlixLatam :
         return listOf(Video(videoUrl, "$prefix Amazon", videoUrl))
     }
 
-    private fun extractNewExtractorLinks(htmlContent: String): List<Pair<String, String>>? {
-        val jsLinksMatch = getFirstMatch("""dataLink = (\[.+?]);""".toRegex(), htmlContent) ?: return null
+    private fun extractNewExtractorLinks(doc: Document, htmlContent: String): List<Pair<String, String>>? {
+        val scriptData = doc.select("script")
+            .asSequence()
+            .map(Element::data)
+            .firstOrNull { it.contains("dataLink") }
 
-        val items = jsLinksMatch.parseAs<List<Item>>()
+        val rawExpression = scriptData?.let {
+            getFirstMatch(DATA_LINK_REGEX, it)
+        } ?: getFirstMatch(DATA_LINK_REGEX, htmlContent)
+
+        val jsonPayload = resolveDataLink(rawExpression) ?: return null
+
+        val items = jsonPayload.parseAs<List<Item>>()
         val idiomas = mapOf("LAT" to "[LAT]", "ESP" to "[CAST]", "SUB" to "[SUB]")
 
         return items.flatMap { item ->
-            val languageCode = idiomas[item.video_language] ?: "unknown"
+            val languageKey = item.video_language?.uppercase() ?: ""
+            val languageCode = idiomas[languageKey] ?: "unknown"
+
             item.sortedEmbeds.mapNotNull { embed ->
                 runCatching {
-                    val decryptedLink = CryptoAES.decrypt(embed.link, "Ak7qrvvH4WKYxV2OgaeHAEg2a5eh16vE")
-                    Pair(decryptedLink, languageCode)
+                    if (!"video".equals(embed.type, ignoreCase = true)) return@mapNotNull null
+
+                    val decryptedLink = decryptEmbedLink(embed.link)
+                    decryptedLink?.let { Pair(it, languageCode) }
                 }.getOrNull()
             }
         }.ifEmpty { null }
     }
 
     private fun getFirstMatch(regex: Regex, input: String): String? = regex.find(input)?.groupValues?.get(1)
+
+    private fun resolveDataLink(rawExpression: String?): String? {
+        if (rawExpression.isNullOrBlank()) return null
+
+        var expr = rawExpression.trim().trimEnd(';')
+
+        fun String.removeOuterCall(prefix: String): String? {
+            if (!startsWith(prefix, ignoreCase = true) || !endsWith(')')) return null
+            val start = indexOf('(')
+            val end = lastIndexOf(')')
+            if (start == -1 || end == -1 || end <= start) return null
+            return substring(start + 1, end).trim()
+        }
+
+        fun String.trimMatchingQuotes(): String {
+            return if ((startsWith('"') && endsWith('"')) || (startsWith('\'') && endsWith('\''))) {
+                substring(1, length - 1)
+            } else {
+                this
+            }
+        }
+
+        while (true) {
+            when {
+                expr.removeOuterCall("JSON.parse") != null -> expr = expr.removeOuterCall("JSON.parse")!!
+                expr.removeOuterCall("window.JSON.parse") != null -> expr = expr.removeOuterCall("window.JSON.parse")!!
+                expr.removeOuterCall("decodeURIComponent") != null -> {
+                    val inner = expr.removeOuterCall("decodeURIComponent")!!.trimMatchingQuotes()
+                    expr = runCatching { URLDecoder.decode(inner, "UTF-8") }.getOrElse { return null }
+                }
+                expr.removeOuterCall("window.decodeURIComponent") != null -> {
+                    val inner = expr.removeOuterCall("window.decodeURIComponent")!!.trimMatchingQuotes()
+                    expr = runCatching { URLDecoder.decode(inner, "UTF-8") }.getOrElse { return null }
+                }
+                expr.removeOuterCall("atob") != null -> {
+                    val inner = expr.removeOuterCall("atob")!!.trimMatchingQuotes()
+                    expr = runCatching { String(Base64.decode(inner, Base64.DEFAULT)) }.getOrElse { return null }
+                }
+                expr.removeOuterCall("window.atob") != null -> {
+                    val inner = expr.removeOuterCall("window.atob")!!.trimMatchingQuotes()
+                    expr = runCatching { String(Base64.decode(inner, Base64.DEFAULT)) }.getOrElse { return null }
+                }
+                else -> break
+            }
+        }
+
+        expr = expr.trim().trimMatchingQuotes()
+
+        return expr.takeIf { it.isNotBlank() }
+    }
+
+    private fun decryptEmbedLink(rawLink: String?): String? {
+        if (rawLink.isNullOrBlank()) return null
+
+        val link = rawLink.trim()
+        if (link.startsWith("http", true)) return link
+
+        CryptoAES.decryptCbcIV(link, AES_KEY)?.takeIf { it.isNotBlank() }?.let { return it }
+        CryptoAES.decrypt(link, AES_KEY).takeIf { it.isNotBlank() }?.let { return it }
+
+        decodeJwtLink(link)?.takeIf { it.isNotBlank() }?.let { return it }
+
+        return null
+    }
+
+    private fun decodeJwtLink(token: String): String? {
+        val segments = token.split('.')
+        if (segments.size < 2) return null
+
+        val payload = segments[1].padBase64Url()
+
+        return runCatching {
+            val decoded = Base64.decode(payload, Base64.URL_SAFE or Base64.NO_WRAP)
+            val element = json.parseToJsonElement(String(decoded))
+            val obj = element.jsonObject
+
+            val link = obj["link"]?.jsonPrimitive?.contentOrNull
+            val nestedLink = obj["data"]?.jsonObject?.get("link")?.jsonPrimitive?.contentOrNull
+
+            link ?: nestedLink
+        }.getOrNull()
+    }
+
+    private fun String.padBase64Url(): String {
+        val padding = (4 - length % 4) % 4
+        return this + "=".repeat(padding)
+    }
 
     // ============================== Filters ===============================
     override val fetchGenres = false
@@ -269,16 +387,17 @@ class FlixLatam :
 
     @Serializable
     data class Item(
-        val file_id: Int,
-        val video_language: String, // Campo nuevo para almacenar el idioma
-        val sortedEmbeds: List<Embed>,
+        val file_id: Int? = null,
+        val video_language: String? = null,
+        val sortedEmbeds: List<Embed> = emptyList(),
     )
 
     @Serializable
     data class Embed(
-        val servername: String,
-        val link: String,
-        val type: String,
+        val servername: String? = null,
+        val link: String? = null,
+        val type: String? = null,
+        val download: String? = null,
     )
 
     override fun List<Video>.sort(): List<Video> {
@@ -306,5 +425,7 @@ class FlixLatam :
         private val PREF_LANG_ENTRIES = arrayOf("[LAT]", "[SUB]", "[CAST]")
         private val PREF_LANG_VALUES = arrayOf("[LAT]", "[SUB]", "[CAST]")
         private val SERVER_LIST = arrayOf("StreamWish", "Uqload", "VidGuard", "StreamHideVid", "Voe")
+        private const val AES_KEY = "Ak7qrvvH4WKYxV2OgaeHAEg2a5eh16vE"
+        private val DATA_LINK_REGEX = """dataLink\s*=\s*([^;]+);""".toRegex(RegexOption.DOT_MATCHES_ALL)
     }
 }