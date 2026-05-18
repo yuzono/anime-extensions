ba5587d22b0cafc5d56ef425bc31178fd1b49336
Amnesia <amnesia@example.com>
2026-05-17 22:16:46 -0400

    fix(anilist): Resolve library update issues

--- src/en/anilist/src/eu/kanade/tachiyomi/animeextension/en/anilist/CoverProviders.kt
+++ src/en/anilist/src/eu/kanade/tachiyomi/animeextension/en/anilist/CoverProviders.kt
@@ -7,20 +7,22 @@ import okhttp3.OkHttpClient
 
 class CoverProviders(private val client: OkHttpClient, private val headers: Headers) {
     fun getMALCovers(malId: String): List<String> {
-        val picturesResponse = client.newCall(
+        val response = client.newCall(
             GET("https://api.jikan.moe/v4/anime/$malId/pictures", headers),
-        ).execute().parseAs<MALPicturesDto>()
+        ).execute()
+        if (!response.isSuccessful) return emptyList()
 
-        return picturesResponse.data.mapNotNull { imgs ->
-            imgs.jpg.let { it.largeImageUrl ?: it.imageUrl ?: it.smallImageUrl }
-        }
+        return response.parseAs<MALPicturesDto>().data?.mapNotNull { imgs ->
+            imgs.jpg?.let { it.largeImageUrl ?: it.imageUrl ?: it.smallImageUrl }
+        } ?: emptyList()
     }
 
     fun getFanartCovers(tvdbId: String, type: String): List<String> {
-        val picturesResponse = client.newCall(
+        val response = client.newCall(
             GET("https://webservice.fanart.tv/v3/$type/$tvdbId?api_key=184e1a2b1fe3b94935365411f919f638", headers),
         ).execute()
+        if (!response.isSuccessful) return emptyList()
 
-        return picturesResponse.parseAs<FanartDto>().tvposter?.map { it.url } ?: emptyList()
+        return response.parseAs<FanartDto>().tvposter?.map { it.url } ?: emptyList()
     }
 }
