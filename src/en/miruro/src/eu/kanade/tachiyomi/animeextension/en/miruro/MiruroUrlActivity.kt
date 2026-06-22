994f53e09a80a1b32878655314b9fb5b25649b7a
Cuong-Tran <16017808+cuong-tran@users.noreply.github.com>
2026-06-23 00:15:53 +0700

    Revert UrlActivitiy

--- src/en/miruro/src/eu/kanade/tachiyomi/animeextension/en/miruro/MiruroUrlActivity.kt
+++ src/en/miruro/src/eu/kanade/tachiyomi/animeextension/en/miruro/MiruroUrlActivity.kt
@@ -5,6 +5,7 @@ import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import kotlin.system.exitProcess
 
 /**
  * Springboard that accepts https://miruro.tv/watch/{anilistId}/{slug} intents
@@ -30,5 +31,6 @@ class MiruroUrlActivity : Activity() {
         }
 
         finish()
+        exitProcess(0)
     }
 }
