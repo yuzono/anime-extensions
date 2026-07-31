b3ae20a89059ed328bd1b7569e2be94a9b74a39d
Alpha-782 <georgesgeorges782@gmail.com>
2026-08-01 01:51:50 +0300

    Add status domain note

--- src/en/reanime/src/eu/kanade/tachiyomi/animeextension/en/reanime/ReAnime.kt
+++ src/en/reanime/src/eu/kanade/tachiyomi/animeextension/en/reanime/ReAnime.kt
@@ -749,6 +749,7 @@ class ReAnime :
     companion object {
         private const val PREF_DOMAIN_KEY = "preferred_domain"
         private val PREF_DOMAIN_ENTRIES = listOf("reanime.to", "reanime.cz")
+        // Status domain: https://restatus.me/
         private val PREF_DOMAIN_VALUES = listOf("https://reanime.to", "https://reanime.cz")
         private const val PREF_DOMAIN_DEFAULT = "https://reanime.to"
         private const val PREF_LANG_KEY = "preferred_lang"
