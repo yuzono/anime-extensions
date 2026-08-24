9cccfe3ec3ed8551d07f640cd4a9ff75bb3571dc
Itsmechinmoy <167056923+itsmechinmoy@users.noreply.github.com>
2026-08-24 19:31:38 +0530

    Remove unused import for newChunkedResponse

--- lib/m3u8server/src/aniyomi/lib/m3u8server/M3u8HttpServer.kt
+++ lib/m3u8server/src/aniyomi/lib/m3u8server/M3u8HttpServer.kt
@@ -11,7 +11,6 @@ import okhttp3.Request
 import org.nanohttpd.protocols.http.IHTTPSession
 import org.nanohttpd.protocols.http.NanoHTTPD
 import org.nanohttpd.protocols.http.response.Response
-import org.nanohttpd.protocols.http.response.Response.newChunkedResponse
 import org.nanohttpd.protocols.http.response.Response.newFixedLengthResponse
 import org.nanohttpd.protocols.http.response.Status
 import java.io.ByteArrayInputStream
