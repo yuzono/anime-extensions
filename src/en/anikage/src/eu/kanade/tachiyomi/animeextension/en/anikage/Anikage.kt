5208e2d3f772e003597ac0acd59797f1d6c13b89
Bilal <bjlegend007@gmail.com>
2026-08-25 20:52:39 +0530

    update in animeDetailsParse

--- src/en/anikage/src/eu/kanade/tachiyomi/animeextension/en/anikage/Anikage.kt
+++ src/en/anikage/src/eu/kanade/tachiyomi/animeextension/en/anikage/Anikage.kt
@@ -142,7 +142,18 @@ class Anikage :
 
         val statusName = soup.selectFirst("span.uppercase.tracking-wider")?.text()
 
+        val title = soup.selectFirst("""meta[property="og:title"]""")
+            ?.attr("content")
+            ?.removeSuffix(" - Watch on Anikage")
+        val description = soup.selectFirst("""meta[property="og:description"]""")?.attr("content")
+        val thumbnail_url = soup.selectFirst("""meta[property="og:image"]""")?.attr("content")
+
         return SAnime.create().apply {
+            if (title != null) {
+                this.title = title
+            }
+            this.description = description
+            this.thumbnail_url = thumbnail_url
             author = authorName
             update_strategy = if (statusName == "Finished") {
                 AnimeUpdateStrategy.ONLY_FETCH_ONCE
