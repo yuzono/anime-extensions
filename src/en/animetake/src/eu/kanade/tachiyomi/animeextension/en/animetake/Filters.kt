b67f75c6274698af9b94f9983dfd9fa0e4baf891
arkhart <arkhart@gmail.com>
2026-09-13 21:25:34 -0700

    minor fix

--- src/en/animetake/src/eu/kanade/tachiyomi/animeextension/en/animetake/Filters.kt
+++ src/en/animetake/src/eu/kanade/tachiyomi/animeextension/en/animetake/Filters.kt
@@ -121,6 +121,8 @@ object Filters {
         )
 
         val YEAR = arrayOf(
+            Pair("2026", "2026"),
+            Pair("2025", "2025"),
             Pair("2024", "2024"),
             Pair("2023", "2023"),
             Pair("2022", "2022"),
